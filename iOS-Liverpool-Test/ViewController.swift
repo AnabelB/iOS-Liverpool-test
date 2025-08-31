68a9fd3936b896e91634035a0d6e9604df585e21
Ana Bernal <diazbernalanabel@gmail.com>
2025-08-31 11:42:58 -0600

    Initial commit: Project created with United Tests

---
+++ iOS-Liverpool-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Liverpool-Test
+//
+//  Created by Ana Bernal on 31/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-Liverpool-TestTests/iOS_Liverpool_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Liverpool_TestTests.swift
+//  iOS-Liverpool-TestTests
+//
+//  Created by Ana Bernal on 31/08/25.
+//
+
+import Testing
+@testable import iOS_Liverpool_Test
+
+struct iOS_Liverpool_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
